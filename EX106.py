0f2f20d460dfbb473e473306e31658cedea5ddba
zion <guzhaoyuan@126.com>
2016-04-22 21:31:57 +0800

    make output beautiful

--- EX106.py
+++ EX106.py
@@ -23,16 +23,15 @@ def parse(respond):
 		return ''
 	if pack[2:4] == 'ff':
 		return [pack[4:6],pack[8:10]]#[id , error]
-		#print 'id='+pack[4:6]
-		#print 'error='+pack[8:10]
 	else:
 		return [pack[2:4],pack[6:8]]
-		#print 'id='+pack[2:4]
-		#print 'error='+pack[6:8]
 
 def scan():
-	for num in range(1,256):
-		ping(num)
+	ids = []
+	for num in range(1,5):
+		if ping(num) != None:
+			ids.append(num)
+	print "find id:"+str(ids)
 
 def ping(id):
 	print "ping id: %d" %id
@@ -47,9 +46,9 @@ def ping(id):
 	ser.write(package)
 	respond = ser.read(size=7)
 	result = parse(respond)
-	#print result
 	if result != '':
 		print "find motor id: %d"%id
+		return id
 
 
 def write(id,instr,*para):
@@ -66,29 +65,5 @@ def write(id,instr,*para):
 	respond = ser.read(size=7)
 	result = parse(respond)
 
-addr = 0x01
-length = 0x05
-instr = 0x03
-parameter1 = 0x1E
-parameter2 = 0x00
-parameter3 = 0x00
-crc = checksum(addr + length + instr + parameter1 + parameter2 + parameter3)
-data = [addr,length,instr,parameter1,parameter2,parameter3,crc]
-package = "".join(map(chr,[0xFF,0xFF] + data))
-
-
-
 ser.flushOutput();
 ser.flushInput();
-#ser.write(package)
-#time.sleep(0.1)
-#ser.write(package)
-
-#while True:
-#respond = ser.read(size=7)
-	#res= [map(ord, x) for x in respond]
-#res= binascii.hexlify(respond) 
-#print res
-	#if res.strip():
-#parse(res)
-#ser.close()
